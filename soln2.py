77ded2fd2bfa98b83053c15a65e445944b942204
Nica Jane Ferrer <nicajaneferrer@yahoo.com>
2016-08-16 15:00:03 +0800

    added soln2.py file

---
+++ soln2.py
@@ -0,0 +1,41 @@
+
+# coding: utf-8
+
+# In[4]:
+
+from randnumber import randnum
+from __future__ import division
+from numpy import random
+
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+
+    print "Exercise 8.1"
+
+    #partA
+    print "Part A: The two random numbers are ", randnum(), "and ", randnum()
+
+
+
+    print "Part B: "
+
+    numlist = []
+    count = 0.0
+    N = int(1e6+1)
+    for n in xrange(0,N):
+        num1 = randnum()
+        num2 = randnum()
+        numlist.append(num1)
+        numlist.append(num2)
+        if num1 == 6 and num2 ==6:
+            count +=1
+    fraction = count/1e6
+    print fraction
+
+
+# In[ ]:
+
+
+
